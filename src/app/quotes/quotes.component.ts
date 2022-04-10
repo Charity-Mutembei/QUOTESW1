2e9f9188861e8d8cdf140827a8330d53d66bb8ff
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-10 14:58:35 +0300

    class linked  to component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,6 +6,16 @@ import { Quotes } from '../quotes'
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quotes [] = [
+    // (name:string, id:number, quote:string, upvote:number, downvote:number)
+    new Quotes('quotes authors name', 1, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 2, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 3, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 4, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 5, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 6, 'quote', 0, 0, ),
+    new Quotes('quotes authors name', 7, 'quote', 0, 0, )
+  ]
 
   constructor() { }
 
