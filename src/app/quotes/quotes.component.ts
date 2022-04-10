a3b19b3cbe8c99b2695930ebeb0569e8c859b150
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-10 21:19:51 +0300

    delete buttons installed

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 // import { globalAgent } from 'http';
-import { Quotes } from '../quotes'
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  styleUrls: ['./quotes.component.css'],
 })
 export class QuotesComponent implements OnInit {
-  quotes: Quotes [] = [
+  quotes: Quotes[] = [
     // (quote:string, id:number, name:string, upvote:number, downvote:number)
     // new Quotes('quote', 1, 'quotes authors name', 0, 0),
     // new Quotes('quote', 1, 'quotes authors name', 0, 0),
@@ -19,17 +19,14 @@ export class QuotesComponent implements OnInit {
     // new Quotes('quote', 1, 'quotes authors name', 0, 0),
   ];
 
-  addNewQuotes(quote:any){
+  addNewQuotes(quote: any) {
     let goalLength = this.quotes.length;
-    quote.id = goalLength+1;
+    quote.id = goalLength + 1;
     // goalLength.completeDate = new Date(quote.completeDate)
-    this.quotes.push(quote)
+    this.quotes.push(quote);
   }
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
